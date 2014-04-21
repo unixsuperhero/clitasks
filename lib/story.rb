be70bbdb2bf9719f227f44345ae9291c5c9af3d8
Joshua Toyota <jearsh@gmail.com>
2014-04-20 23:39:56 -0400

    Story accessors fixed; story name passed as arg; concise class definitions; Story.build for cleaner implemention

--- lib/story.rb
+++ lib/story.rb
@@ -2,50 +2,66 @@ require 'pp'
 
 module CliTasks
   class Story
-    attr_reader :status, :points, :name, :description, :created_by, :assigned_to, :comments, :tags
-    def initialize(&block)
-      instance_eval &block
+    attr_reader :id, :status, :points, :name, :description, :created_by, :assigned_to, :comments, :tags
+    def initialize(builder)
+      builder.instance_variables.each{|name| instance_variable_set name, builder.instance_variable_get(name) }
     end
 
-    def status(status)
-      @status = status
-    end
-
-    def points(points)
-      @points = points
-    end
-
-    def name(name)
-      @name = name
-    end
-
-    def description(description)
-      @description = description
-    end
-
-    def created_by(created_by)
-      @created_by = created_by
-    end
-
-    def assigned_to(*args)
-      @assigned_to = args
-    end
-
-    def comment(author, body)
-      comments << {author: author, body: body}
-    end
-
-    def tags(*args)
-      @tags = args
-    end
-
-    def comments
-      @comments ||= []
+    def self.build(name, &block)
+      Story.new StoryReader.new(name, &block)
     end
   end
 
-  def story(&block)
-    stories << Story.new(&block)
+  class SimpleDSL
+    class << self
+      def values(*names)
+        names.each do |name|
+          define_method(name) do
+            "#{name}".to_sym
+          end
+        end
+      end
+
+      def fields(*names)
+        names.each do |name|
+          define_method(name) do |val|
+            instance_variable_set "@#{name}".to_sym, val
+          end
+        end
+      end
+
+      def groups(*names)
+        names.each do |name|
+          define_method(name) do |*vals|
+            instance_variable_set "@#{name}".to_sym, vals
+          end
+        end
+      end
+
+      def custom(*names, &block)
+        names.each do |name|
+          define_method(name, &block)
+        end
+      end
+    end
+  end
+
+  class StoryReader < SimpleDSL
+    def initialize(name, &block)
+      @name = name
+      instance_eval &block
+    end
+
+    values :queued, :started, :finished,
+           :accepted, :rejected, :delivered
+
+    fields :id, :status, :points, :description, :created_by
+    groups :assigned_to, :tags
+    custom :comment do |author, body| (@comments ||= []) << {author: author, body: body} end
+  end
+
+  def story(name, &block)
+    stories << Story.build(name, &block)
   end
 
   def stories
