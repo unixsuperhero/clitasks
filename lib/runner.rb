a26822eb1a40eaba4989d4f68c30dd5a47188154
Joshua Toyota <jearsh@gmail.com>
2014-04-21 00:36:40 -0400

    simplifies file loader in Runner

--- lib/runner.rb
+++ lib/runner.rb
@@ -2,8 +2,7 @@ module CliTasks
   class Runner
     def self.run(*files)
       files.flat_map{|file|
-        file = [file, '/**/*'].join if File.directory?(file)
-        Dir[file]
+        Dir[File.directory?(file) && [file,'/**/*'].join || file]
       }.map{|file| load File.expand_path(file) }
     end
   end
