b3976ea1991c263826b95ef46fb1aa6a502a91ee
Joshua Toyota <jearsh@gmail.com>
2014-04-21 00:58:03 -0400

    adds more stories

---
+++ stories/index/idlujhlssugljjlffuag.rb
@@ -0,0 +1,14 @@
+story %q(use YYYYmmddHHMMSS for a story's id) do
+  id 'idlujhlssugljjlffuag'
+  status queued
+  points 1
+  # created_by :unassigned
+  # assigned_to :unassigned
+  # tags :one, :two
+  #
+  # description <<-DESCRIPTION
+  # DESCRIPTION
+  #
+  # comment :username, <<-COMMENT
+  # COMMEND
+end
---
+++ stories/index/igdljhahadkjugfakhah.rb
@@ -0,0 +1,14 @@
+story %q(use percent notation for story names - instead of escaping quotes) do
+  id 'igdljhahadkjugfakhah'
+  status queued
+  points 1
+  # created_by :unassigned
+  # assigned_to :unassigned
+  # tags :one, :two
+  #
+  # description <<-DESCRIPTION
+  # DESCRIPTION
+  #
+  # comment :username, <<-COMMENT
+  # COMMEND
+end
