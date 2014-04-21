70604c247aa2faf248cf0ea8990b7bf76ff880a2
Joshua Toyota <jearsh@gmail.com>
2014-04-21 02:03:20 -0400

    adds story

---
+++ stories/index/iggahsajlgguagfdkkus.rb
@@ -0,0 +1,14 @@
+story %q(refactor LinkBuilder#link) do
+  id 'iggahsajlgguagfdkkus'
+  status queued
+  points 1
+  # created_by :unassigned
+  # assigned_to :unassigned
+  # tags :one, :two
+  #
+  # description <<-DESCRIPTION
+  # DESCRIPTION
+  #
+  # comment :username, <<-COMMENT
+  # COMMEND
+end
