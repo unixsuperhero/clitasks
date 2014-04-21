567ea777ee14123ead0d0a69efcd93cacee9c951
Joshua Toyota <jearsh@gmail.com>
2014-04-21 01:03:23 -0400

    adds stories

---
+++ stories/index/ighfglhkldsksldkjjfh.rb
@@ -0,0 +1,14 @@
+story %q(paused status - for started, but unmerged stories) do
+  id 'ighfglhkldsksldkjjfh'
+  status queued
+  points 1
+  # created_by :unassigned
+  # assigned_to :unassigned
+  # tags :one, :two
+  #
+  # description <<-DESCRIPTION
+  # DESCRIPTION
+  #
+  # comment :username, <<-COMMENT
+  # COMMEND
+end
