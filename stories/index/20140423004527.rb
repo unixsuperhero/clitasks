d0f346d05c5db17bc518b164117dbf565f977f28
Joshua Toyota <jearsh@gmail.com>
2014-04-23 00:46:26 -0400

    starts story

---
+++ stories/index/20140423004527.rb
@@ -0,0 +1,7 @@
+story %q(default sort order for stories should be by status) do
+  status started
+  points 2
+  created_by :unixsuperhero
+  assigned_to :unixsuperhero
+  tags *%w(refactor cmd command list)
+end
