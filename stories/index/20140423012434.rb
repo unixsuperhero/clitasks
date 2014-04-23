a3bf0fc7c30dd7d5582c6e216997024da8cb9efa
Joshua Toyota <jearsh@gmail.com>
2014-04-23 02:46:55 -0400

    stories for add-ons to integrate with 3rd party sites

---
+++ stories/index/20140423012434.rb
@@ -0,0 +1,14 @@
+story %q(project: addon to import/sync with trac) do
+  status queued
+  points 1
+  created_by :unixsuperhero
+  assigned_to :unassigned
+  tags *%w(project trac importers integration)
+
+  description <<-DESCRIPTION
+    One of the many 3rd party site integrators
+
+    Tools like this, that can import and sync with an existing site/service
+    will make adopting clitasks easier and less farfetched.
+  DESCRIPTION
+end
