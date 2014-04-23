6087fbaa3d4f987283cfcb93f6a1460c9ce03a29
Joshua Toyota <jearsh@gmail.com>
2014-04-23 00:10:50 -0400

    adds more stories

---
+++ stories/index/20140423000507.rb
@@ -0,0 +1,15 @@
+story %q(create options for clit [list] to sort by 1+ id, points, name/title, status, or created_at) do
+  status queued
+  points 2
+  created_by :unixsuperhero
+  assigned_to :unixsuperhero
+  tags :cmd, :command
+
+  description <<-DESCRIPTION
+    create:
+      $> clit list
+
+    use clit [blank]
+    as clit help
+  DESCRIPTION
+end
---
+++ stories/index/20140423000844.rb
@@ -0,0 +1,16 @@
+story %q(strip [_[^[:word:]]+ from the beginning and end of symlink names) do
+  status queued
+  points 1
+  created_by :unixsuperhero
+  assigned_to :unassigned
+  tags :cmd, :command, :rebuild
+
+  description <<-DESCRIPTION
+    Right now some of the symlinks' names are:
+      _something_holy_wow
+      _even_worse_
+      asdf_asdf_
+
+    This is just ugly and unnecessary
+  DESCRIPTION
+end
