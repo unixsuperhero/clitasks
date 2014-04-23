19301a1de47714a268b12e5cf6c5e0aad9adf44e
Joshua Toyota <jearsh@gmail.com>
2014-04-23 00:44:03 -0400

    adds stories

---
+++ stories/index/20140423003427.rb
@@ -0,0 +1,17 @@
+story %q(separate lib/story.rb into one file per class) do
+  status queued
+  points 2
+  created_by :unixsuperhero
+  assigned_to :unassigned
+  tags *%w(refactor gem) # *%w(example example_two)
+
+  description <<-DESCRIPTION
+    each class in lib/story.rb should be in its' own file
+
+    the classes are:
+    grep 'class ' lib/story.rb
+    # class Story
+    # class SimpleDSL
+    # class StoryReader < SimpleDSL
+  DESCRIPTION
+end
---
+++ stories/index/20140423003655.rb
@@ -0,0 +1,19 @@
+story %q(lib/'s structure should match namespaces; one file should load the whole env (see: any gem)) do
+  status queued
+  points 1
+  created_by :unixsuperhero
+  assigned_to :unassigned
+  tags *%w(gem setup refactor)
+
+  description <<-DESCRIPTION
+    lib/* should reflect the namespaces
+
+    the toplevel should have lib/clitasks.rb which is responsible for requiring
+    all other lib/**.rb files.  this way, whenever a script needs to use
+    clitasks, it is just requiring one file and not a shitload.  also, all the
+    other files in lib/ won't have to worry about requiring each other
+
+    also, it could possibly use autoload, that way there aren't any issues with
+    one class requiring another before the vm knows where everything is.
+  DESCRIPTION
+end
