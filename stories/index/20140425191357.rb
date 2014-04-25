c3fc35d8bbd5e52f91eefb555df823a625d3cd76
Joshua Toyota <jearsh@gmail.com>
2014-04-25 19:15:52 -0400

    adds: task create <title> not requiring title to be in quotes as the next argument

---
+++ stories/index/20140425191357.rb
@@ -0,0 +1,15 @@
+story %q("task create <title>" does not require title to be a single argument (in quotes)) do
+  status started
+  points 1
+  created_by :unixsuperhero
+  assigned_to :unixsuperhero
+  tags *%w(cmd command usability) # *%w(example example_two)
+
+  description <<-DESCRIPTION
+    basically just do an:
+
+      args.join(' ')
+
+    when generating the story's name
+  DESCRIPTION
+end
