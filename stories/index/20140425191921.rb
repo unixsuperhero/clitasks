2219fe01bdcae590c75604be19a513952ecf7949
Joshua Toyota <jearsh@gmail.com>
2014-04-25 19:20:38 -0400

    adds: task create should rebulid symlinks at the end

---
+++ stories/index/20140425191921.rb
@@ -0,0 +1,7 @@
+story %q(task create title rebuilds symlinks at the end) do
+  status started
+  points 1
+  created_by :unixsuperhero
+  assigned_to :unixsuperhero
+  tags *%w(cmd command bug) # *%w(example example_two)
+end
