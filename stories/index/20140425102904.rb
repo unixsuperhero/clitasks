0a2c8073a78acd15d9d2375c35d25c0a21bff1a5
Joshua Toyota <jearsh@gmail.com>
2014-04-25 19:00:54 -0400

    adds first bug

---
+++ stories/index/20140425102904.rb
@@ -0,0 +1,12 @@
+story %q(bug: fix title not being interpolated in new stories) do
+  status queued
+  points 1
+  created_by :unixsuperhero
+  assigned_to :unassigned
+  tags *%w() # *%w(example example_two)
+
+  description <<-DESCRIPTION
+    When I extracted the new story template into a new file, it broke the title
+    interpolation.
+  DESCRIPTION
+end
