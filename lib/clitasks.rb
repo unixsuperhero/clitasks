3afe819d733f31e5fbc3de2a62c9a5fae870e47d
Joshua Toyota <jearsh@gmail.com>
2014-04-26 16:42:51 -0400

    created clitasks loader

---
+++ lib/clitasks.rb
@@ -0,0 +1,10 @@
+require './lib/clitasks/world'
+require './lib/clitasks/story'
+require './lib/clitasks/runner'
+require './lib/clitasks/link_builder'
+require './lib/clitasks/viewer'
+require './lib/clitasks/commands'
+
+module CliTasks
+end
+
