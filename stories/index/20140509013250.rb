ad8ea3b1238aeb1f0cb8d720d346f5f0a8176653
Joshua Toyota <jearsh@gmail.com>
2014-05-09 01:35:30 -0400

    starts: task version command

---
+++ stories/index/20140509013250.rb
@@ -0,0 +1,14 @@
+story %q(task version shows the gems current version) do
+  status started
+  points 1
+  created_by 'unixsuperhero'
+  assigned_to :unassigned
+  tags *%w(command version bin)
+
+  description %q(
+  running:
+    $> task version
+
+  will show the gem's version
+  )
+end
