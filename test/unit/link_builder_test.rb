de822f341e8d5b809ba2888d5a3c268de8b2c740
Joshua Toyota <jearsh@gmail.com>
2014-05-08 22:40:06 -0400

    adds rake test and first test

---
+++ test/unit/link_builder_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require './lib/clitasks'
+
+describe CliTasks::LinkBuilder do
+  describe '#link' do
+    it 'should make any intermediate directories'
+    it 'should find the relative_path from the symlink to the links target'
+  end
+end
